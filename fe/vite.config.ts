70e2ddf1881d7a5b756cf4d5d2e03a7f56052d03
5dbwat4 <me@5dbwat4.top>
2024-11-10 17:06:44 +0800

    Setting up fe

---
+++ fe/vite.config.ts
@@ -0,0 +1,39 @@
+import { defineConfig } from 'vite'
+import { fileURLToPath, URL } from 'node:url'
+
+// import { defineConfig } from 'vite'
+import Vue from '@vitejs/plugin-vue'
+import VueJsx from '@vitejs/plugin-vue-jsx'
+
+// import { defineConfig } from 'vite'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver,NaiveUiResolver } from 'unplugin-vue-components/resolvers'
+import VueRouter from 'unplugin-vue-router/vite'
+import Pages from 'vite-plugin-pages';
+import react from '@vitejs/plugin-react-swc'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [    
+    VueRouter(),
+    Pages({
+      dirs: 'src/pages', 
+      exclude: ['**/components/*.vue']
+    }),
+    Vue(),
+    VueJsx(),
+    AutoImport({
+      resolvers: [ElementPlusResolver(),NaiveUiResolver()],
+    }),
+    Components({
+      resolvers: [ElementPlusResolver(),NaiveUiResolver()],
+    }),
+    react()
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
+})
