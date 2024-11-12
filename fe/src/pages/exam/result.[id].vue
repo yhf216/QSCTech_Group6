291b0e194971b920ff93fb53897514639dde45d0
5dbwat4 <me@5dbwat4.top>
2024-11-12 21:58:36 +0800

    Add[fe]: exam/result part

--- fe/src/pages/exam/result.[id].vue
+++ fe/src/pages/exam/result.[id].vue
@@ -1,5 +1,15 @@
 <template>
+  <n-card
+    style="width: 70%; min-width: 800px; max-width: 100%; margin: auto auto"
+    hoverable
+  >
+<n-h1 style="text-align: center; margin-top: 20px">Exam Result</n-h1>
 
+
+<n-p>Congrats! You scored {{result.score}} out of {{result.total_marks}} marks.</n-p>
+
+
+</n-card>
 
 </template>
 <script setup>
