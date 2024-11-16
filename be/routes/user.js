f4604c4ad7447b9585dc4149f0184f1452461c06
5dbwat4 <me@5dbwat4.top>
2024-11-16 13:28:38 +0800

    Update[be]: response format

--- be/routes/user.js
+++ be/routes/user.js
@@ -10,14 +10,14 @@ export default (app, db) => {
     app.post('/user/login', async (req, res) => {    
         const { username, password } = req.body;
         if(typeof username!== "string" || typeof password!== "string") {
-            return res.status(400).json({ error: 'Username and password must be strings' });
+            return res.status(200).json({ message: 'Username and password must be strings' });
         }
         if (!username ||!password) {
-            return res.status(400).json({ error: 'Username and password are required' });
+            return res.status(200).json({ message: 'Username and password are required' });
         }
         const user = await db.get(`SELECT * FROM users WHERE username =? AND password =?`, [username, password])    ;
         if (!user) {
-            return res.status(401).json({ error: 'Invalid username or password' });
+            return res.status(200).json({ message: 'Invalid username or password' });
         }
         HandleJWT(user, res);
         res.json({ message: 'Login successful', user });
@@ -25,16 +25,16 @@ export default (app, db) => {
     app.post('/user/register', async (req, res) => {    
         const { username, password /* password is SHA256 hashed */ } = req.body;
         if(typeof username!== "string" || typeof password!== "string") {
-            return res.status(400).json({ error: 'Username and password must be strings' });
+            return res.status(200).json({ message: 'Username and password must be strings' });
         }
         if (!username ||!password) {
-            return res.status(400).json({ error: 'Username and password are required' });
+            return res.status(200).json({ message: 'Username and password are required' });
         }
         if (/^[a-f0-9]{64}$/.test(password) === false) {
-            return res.status(400).json({ error: 'Password invalid' });
+            return res.status(200).json({ message: 'Password invalid' });
         }
         if (await db.get(`SELECT * FROM users WHERE username =?`, [username])) {
-            return res.status(400).json({ error: 'Username already exists' });
+            return res.status(200).json({ message: 'Username already exists' });
         }
         const user = await db.run(`INSERT INTO users (username, password, created_at) VALUES (?,?, CURRENT_TIMESTAMP)`, [username, password]);
         HandleJWT(user, res);
